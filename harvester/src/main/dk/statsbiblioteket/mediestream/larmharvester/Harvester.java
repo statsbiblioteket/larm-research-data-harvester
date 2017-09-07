93e6b23b60e1c35ba52b3afef223040f0a868416
Knud Åge Hansen <kaah@statsbiblioteket.dk>
2017-09-07 10:14:08 +0200

    Added METS generator from example

--- harvester/src/main/dk/statsbiblioteket/mediestream/larmharvester/Harvester.java
+++ harvester/src/main/dk/statsbiblioteket/mediestream/larmharvester/Harvester.java
@@ -1,13 +1,16 @@
 package main.dk.statsbiblioteket.mediestream.larmharvester;
 
-import au.edu.apsr.mtk.base.METS;
-import au.edu.apsr.mtk.base.METSException;
-import au.edu.apsr.mtk.base.METSWrapper;
-import au.edu.apsr.mtk.base.MetsHdr;
+import au.edu.apsr.mtk.base.*;
 import com.opencsv.CSVReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -15,8 +18,11 @@ import java.net.URLEncoder;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.TimeZone;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -240,12 +246,13 @@ public class Harvester {
             while ((line = reader.readNext()) != null) {
                 for (int i = 0; i<15; i++) {
                     System.out.print(line[i] + " ");
-                    //writeMETS();
-
                 }
                 System.out.println();
+                writeMETS("./test/data/METSfile.xml");
             }
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (METSException e) {
             e.printStackTrace();
         }
     }
@@ -261,7 +268,135 @@ public class Harvester {
         mets.setType("investigation");
 
         MetsHdr mh = mets.newMetsHdr();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        String currentTime = df.format(cal.getTime());
+        mh.setCreateDate(currentTime);
+        mh.setLastModDate(currentTime);
 
+        Agent agent = mh.newAgent();
+        agent.setRole("CREATOR");
+        agent.setType("OTHER");
+        agent.setName("SampleMETSBuild");
+
+        mh.addAgent(agent);
+
+        mets.setMetsHdr(mh);
+
+        DmdSec dmd = mets.newDmdSec();
+        dmd.setID("J-1");
+        MdWrap mdw = dmd.newMdWrap();
+        mdw.setMDType("MODS");
+        try {
+            mdw.setXmlData(createMODS("Structure of a Thermophilic Serpin in the Native State", "experiment").getDocumentElement());
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        dmd.setMdWrap(mdw);
+
+        mets.addDmdSec(dmd);
+
+        DmdSec dmd2 = mets.newDmdSec();
+        dmd2.setID("J-2");
+        MdWrap mdw2 = dmd2.newMdWrap();
+        mdw2.setMDType("MODS");
+        try {
+            mdw2.setXmlData(createMODS("APS Source 125 Deg (includes xfiles, denzo)", "dataset").getDocumentElement());
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        dmd2.setMdWrap(mdw2);
+
+        mets.addDmdSec(dmd2);
+
+        FileSec fs = mets.newFileSec();
+        FileGrp fg = fs.newFileGrp();
+        fg.setUse("original");
+        au.edu.apsr.mtk.base.File f = fg.newFile();
+        f.setID("F-1");
+        f.setSize(2097152000);
+        f.setMIMEType("application/x-bzip");
+        f.setOwnerID("de.tar.bz.0");
+        f.setChecksum("498d584f08389d40cff70c4adf0659ff");
+        f.setChecksumType("MD5");
+
+        FLocat fl = f.newFLocat();
+        fl.setHref("http://localhost/myapp/get/XTAL_DATASET_de.tar.bz.0");
+        fl.setLocType("URL");
+
+        f.addFLocat(fl);
+        fg.addFile(f);
+
+        au.edu.apsr.mtk.base.File f2 = fg.newFile();
+        f2.setID("F-2");
+        f2.setSize(65193743);
+        f2.setMIMEType("application/x-bzip");
+        f2.setOwnerID("de.tar.bz.1");
+        f2.setChecksum("940faa9ab023cb0d42ef103a34b8c5bd");
+        f2.setChecksumType("MD5");
+
+        FLocat fl2 = f2.newFLocat();
+        fl2.setHref("http://localhost/myapp/get/XTAL_DATASET_de.tar.bz.1");
+        fl2.setLocType("URL");
+
+        f2.addFLocat(fl2);
+        fg.addFile(f2);
+
+        fs.addFileGrp(fg);
+        mets.setFileSec(fs);
+
+        StructMap sm = mets.newStructMap();
+        mets.addStructMap(sm);
+
+        Div d = sm.newDiv();
+        d.setType("investigation");
+        d.setDmdID("J-1");
+        sm.addDiv(d);
+
+        Div d2 = d.newDiv();
+        d2.setType("dataset");
+        d2.setDmdID("J-2");
+        d.addDiv(d2);
+
+        Fptr fp = d2.newFptr();
+        fp.setFileID("F-1");
+        d2.addFptr(fp);
+
+        Fptr fp2 = d2.newFptr();
+        fp2.setFileID("F-2");
+        d2.addFptr(fp2);
+
+        try {
+            mw.validate();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mw.write(System.out);
+
+    }
+
+    static private Document createMODS(String title, String genre) throws ParserConfigurationException
+    {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.newDocument();
+        Element root = doc.createElementNS("http://www.loc.gov/mods/v3", "mods");
+        doc.appendChild(root);
+
+        Element ti = doc.createElement("titleInfo");
+        Element t = doc.createElement("title");
+        t.setTextContent(title);
+        ti.appendChild(t);
+        root.appendChild(ti);
+
+        Element g = doc.createElement("genre");
+        g.setTextContent(genre);
+        root.appendChild(g);
+
+        return doc;
     }
 
     /**
